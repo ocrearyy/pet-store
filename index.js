1a3628d22aeabd5e500421fbc33c03f96a4e7ec3
oshanedesign <oshane.design@gmail.com>
2022-05-27 06:14:12 -0500

    fixed deploying issues

--- index.js
+++ index.js
@@ -1,37 +1,40 @@
-const express = require('express');
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
 const app = express();
-const path = require('path')
-const redditData = require('./views/data.json')
+const path = require("path");
+const redditData = require("./views/data.json");
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, "/public")));
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'))
-app.get('/', (req, res) => {
-  res.render('home')
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
-app.get('/cats', (req, res) => {
-  const cats = ['Blue', 'Rocket', 'Monty', 'STephanie', 'Winston']
-  res.render('cats', { cats });
-})
+app.get("/cats", (req, res) => {
+  const cats = ["Blue", "Rocket", "Monty", "STephanie", "Winston"];
+  res.render("cats", { cats });
+});
 
-app.get('/r/:submix', (req, res) => {
+app.get("/r/:submix", (req, res) => {
   const { submix } = req.params;
   const data = redditData[submix];
-  if(data){
-    res.render('mixology', { ...data }); 
+  if (data) {
+    res.render("mixology", { ...data });
   } else {
-res.render('notfound', { submix } )
+    res.render("notfound", { submix });
   }
-})
+});
 
-app.get('/subscribe', (req, res) => {
-  const num = Math.floor((Math.random()*10
-   ) +1 )
-  res.render('subscription', { num })
-})
-
-app.listen(3000, () => {
-    console.log('LISTENING ON PORT 3000')
-})
+app.get("/subscribe", (req, res) => {
+  const num = Math.floor(Math.random() * 10 + 1);
+  res.render("subscription", { num });
+});
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("LISTENING ON PORT 3000");
+});
